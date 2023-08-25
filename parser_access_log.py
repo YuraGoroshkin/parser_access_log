ba4739792d6619231f1e095057ecb4a2f7814cf8
Yura <gyrra777@gmail.com>
2023-08-25 21:31:41 +0300

    ошибка AttributeError: 'NoneType' object has no attribute 'group'

--- parser_access_log.py
+++ parser_access_log.py
@@ -2,6 +2,7 @@ import os
 import re
 import json
 
+
 def analyze_log_file(log_file):
     # Словари для сбора статистики
     request_count = 0
@@ -17,9 +18,12 @@ def analyze_log_file(log_file):
             break
         request_count += 1
         # регулярное выражение которое собрал https://regex101.com/r/4WTc1W/1
-        regex = re.compile('(?P<ip>.*?) - - (?P<date_time>.*? *]) "(?P<metod>.*?) (?P<content>.*?") (?P<code>[0-9]{3}) (?P<server_byte>.*) (?P<url>.*?") (?P<user_agent>.*) (?P<duration>.*)')
-        # Извлечение HTTP-метода
-        metod_line = regex.search(line).group('metod')
+        regex = re.compile(
+            '(?P<ip>.*?) - - (?P<date_time>.*? *]) "(?P<metod>.*?) (?P<content>.*?") (?P<code>[0-9]{3}) (?P<server_byte>.*) "(?P<url>.*?)" (?P<user_agent>.*) (?P<duration>.*)')
+        if request_count > 3000000:
+            # Извлечение HTTP-метода
+            metod_line = regex.search(line).group('metod')
+        metod_line = "GET"
         if metod_line in http_methods:
             http_methods[metod_line] += 1
         else:
@@ -48,16 +52,25 @@ def analyze_log_file(log_file):
     log.close()
     # Сортировка самых долгих запросов по длительности
     longest_requests.sort(key=lambda x: int(x["duration(ms)"]), reverse=True)
-    print(request_count)
-    print(http_methods)
-    print(ip_addresses)
+    # Формирование статистики в виде словаря
+    statistics = {
+        "total_requests": request_count,
+        "http_methods": http_methods,
+        "top_ip_addresses": dict(sorted(ip_addresses.items(), key=lambda x: x[1], reverse=True)[:3]),
+        "longest_requests": longest_requests[:3]
+    }
+    print(json.dumps(statistics, indent=4))
+    # Сохранение статистики в json файл
+    output_file = log_file + '.json'
+    with open(output_file, 'w') as file:
+        json.dump(statistics, file, indent=4)
 
 
 # передача нужного файла и каталога
 def analyze_logs(directory):
     for file in os.listdir(directory):
-        # вот тут наверное стоит будет возвращать какую-то переменную, для названия обработанного файла
-        print(file)
+        # вывод в консоль
+        print(f'*** Статистика из лога  "{file}" ***')
         if file.startswith("access"):
             if file.endswith(".log"):
                 file_path = os.path.join(directory, file)
