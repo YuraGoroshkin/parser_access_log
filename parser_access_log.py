f28f19b2cedeb3db872205975848215bfffc3dac
Yura <gyrra777@gmail.com>
2023-08-25 20:53:55 +0300

    по записывал всякого

--- parser_access_log.py
+++ parser_access_log.py
@@ -1,6 +1,6 @@
 import os
 import re
-
+import json
 
 def analyze_log_file(log_file):
     # Словари для сбора статистики
@@ -18,10 +18,39 @@ def analyze_log_file(log_file):
         request_count += 1
         # регулярное выражение которое собрал https://regex101.com/r/4WTc1W/1
         regex = re.compile('(?P<ip>.*?) - - (?P<date_time>.*? *]) "(?P<metod>.*?) (?P<content>.*?") (?P<code>[0-9]{3}) (?P<server_byte>.*) (?P<url>.*?") (?P<user_agent>.*) (?P<duration>.*)')
+        # Извлечение HTTP-метода
+        metod_line = regex.search(line).group('metod')
+        if metod_line in http_methods:
+            http_methods[metod_line] += 1
+        else:
+            http_methods[metod_line] = 1
+        # Извлечение IP адреса
         ip_line = regex.search(line).group('ip')
-        print(ip_line)
+        if ip_line in ip_addresses:
+            ip_addresses[ip_line] += 1
+        else:
+            ip_addresses[ip_line] = 1
+        # длительность запроса
+        duration_line = regex.search(line).group('duration')
+        # url
+        url_line = regex.search(line).group('url')
+        # время запроса
+        date_time_line = regex.search(line).group('date_time')
+        request_info = {
+            "method": metod_line,
+            "url": url_line,
+            "ip": ip_line,
+            "duration(ms)": duration_line,
+            "timestamp": date_time_line
+        }
+        # Добавление запроса в список самых долгих запросов
+        longest_requests.append(request_info)
     log.close()
+    # Сортировка самых долгих запросов по длительности
+    longest_requests.sort(key=lambda x: int(x["duration(ms)"]), reverse=True)
     print(request_count)
+    print(http_methods)
+    print(ip_addresses)
 
 
 # передача нужного файла и каталога
